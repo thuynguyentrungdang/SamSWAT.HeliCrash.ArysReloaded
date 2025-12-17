43a408d13174af936aa9a43daa6f619b1860e2c7
Arys <arys.steam@gmail.com>
2025-12-17 22:40:17 +0700

    chore: removes unused method

--- project/HeliCrash.Core/Utils/LifetimeScopeExtensions.cs
+++ project/HeliCrash.Core/Utils/LifetimeScopeExtensions.cs
@@ -1,6 +1,3 @@
-using System;
-using Comfort.Common;
-using EFT;
 using UnityEngine;
 using VContainer.Unity;
 
@@ -25,26 +22,5 @@ public static class LifetimeScopeExtensions
         child.parentReference.Object = parentScope;
         gameObject.SetActive(true);
         return child;
-    }
-
-    public static RaidLifetimeScope GetRaidLifetimeScope()
-    {
-        GameWorld gameWorld = Singleton<GameWorld>.Instance;
-        if (gameWorld == null)
-        {
-            throw new NullReferenceException(
-                $"GameWorld is null. Please only invoke {nameof(GetRaidLifetimeScope)} during a raid!"
-            );
-        }
-
-        var raidLifetimeScope = gameWorld.GetComponentInChildren<RaidLifetimeScope>();
-        if (raidLifetimeScope == null)
-        {
-            throw new NullReferenceException(
-                $"{nameof(RaidLifetimeScope)} doesn't exist when it should!"
-            );
-        }
-
-        return raidLifetimeScope;
     }
 }
